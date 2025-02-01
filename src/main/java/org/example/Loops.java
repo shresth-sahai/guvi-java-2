f72fd75fbf27561d7d2d324cd263f98ba53be515
shresth-sahai <shresth0407@gmail.com>
2025-02-01 10:46:06 +0530

    added break continue + loops theory

---
+++ src/main/java/org/example/Loops.java
@@ -0,0 +1,61 @@
+//package org.example;
+//
+//public class Loops {
+//// entry controlled loop ->for -> know  the no of iterations
+//    // int i=0 , i<=5 i++
+////    int i;
+////    for(i=0;i<=5;i++){
+////        System.out.print(i);
+////    }
+////
+////      for(i=0;i<=5;i++;){
+////        System.out.print(i);
+////    }
+//    // entry while
+//
+////    int i=1;
+////    while(i<=5){
+////        System.out.print(i);
+////    }
+////
+////    // do while -> exit controlled -> at least one guranteed execution happens
+////    int j=1;
+////    do{
+////        System.out.print(j);
+////        j++;
+////    }while(j<=5);
+////    // j=6
+////
+////    // for each
+////    int[] numbers={10,20,30,40,50};
+////
+////    // 10,20,30,40,50
+////    for(int num:numbers){
+////        System.out.print(num);// 10 20 30 40 50  for(int i=0;i<5;i++) print(numbers[i])
+////    }
+//
+//// i =1, 2 , 3
+//    // 1 2 Breaking out
+//for(int i=1;i<=5;i++){
+//    if(i==3){
+//        System.out.print("Breaking out");
+//        break;
+//    }
+//        System.out.print(i);
+//    }
+//}
+//// break is used to terminate execution prematurely while continue skips the current one moves to next
+//
+//for(int j=1;j<=5;i++){
+//        if(j==3){
+//        System.out.print("Skippping out");
+//        continue;
+//        }
+//        System.out.print(j);
+//        }
+//        }
+//// 1 2 skipping out 4 5
+//
+//
+//
+//
---
+++ src/main/java/org/example/brekacontinue.java
@@ -0,0 +1,47 @@
+//package org.example;
+//
+//public class brekacontinue {
+//
+//    for (int i = 1; i <= 10; i++) {
+//        if (i == 5) {
+//            System.out.println("Stopping at " + i);
+//            break;
+//        }
+//        System.out.print(i + " ");
+//    }
+//
+//
+//        for (int i = 1; i <= 10; i++) {
+//        if (i == 5) {
+//            System.out.println("Skipping " + i);
+//            continue;
+//        }
+//        System.out.print(i + " ");
+//    }
+//
+//
+//    int[] numbers = {3, 7, 9, 10, 15, 18};
+//
+//        for (int num : numbers) {
+//        if (num % 2 == 0) {
+//            System.out.println("First even number found: " + num);
+//            break;
+//        }
+//    }
+//
+//         for (int i = 1; i <= 10; i++) {
+//        if (i % 2 == 0) {
+//            continue;  // Skips even numbers
+//        }
+//        System.out.print(i + " ");
+//    }
+//
+//    int[] numbers = {4, 7, 12, -5, 9, -8};
+//
+//        for (int num : numbers) {
+//        if (num < 0) {
+//            System.out.println("First negative number: " + num);
+//            break;  // Stops at the first negative number
+//        }
+//    }
+//}
