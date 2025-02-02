bbbaae91135a086998e510abdc0ea5c5a30fc050
shresth-sahai <shresth0407@gmail.com>
2025-02-02 10:18:27 +0530

    Array intro

---
+++ src/main/java/org/example/Arrays.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Arrays {
+    // array is data structure -> fized size elements are of same type
+    int [] arr={1,2,3,4,5};
+    int[][] matrix={{1,2,3},{4,5,6}};// 2 d array
+    /*
+
+    1,2,3,4,5
+    0 1 2 4 5
+    arr[0] -> 1
+    arr[1] = 2
+
+
+     */
+    // jagged array -> each row has diff no of cols
+    int[][] matrix1={{1,2,},{4,5,6},{7}};
+    int [] arr1;
+    arr1=new int[5];
+    arr1[0]=12;
+    arr1[1]=13;
+
+
+    int [][] m=new int[3][3];
+    m[0][0]=1;
+
+    int [][] j =new int[3][];
+
+    j[0]=new int[2];
+    j[1]=new int[4];
+
+
+
+
+}
