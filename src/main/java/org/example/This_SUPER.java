5d37c6bbf59f650f025511feb1ae2d68a6aa8aad
shresth-sahai <shresth0407@gmail.com>
2025-02-22 11:08:41 +0530

    this+super

---
+++ src/main/java/org/example/This_SUPER.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class This_SUPER {
+//    public Person{
+//        String name;
+//        int age;
+//
+//    public Person(String name,int age){
+//// inorder to refer to instance var we use this
+//            this.name=name;
+//            this.age=age;
+//        }
+//    }
+   // this.methodName -> call another method in the same class
+//    class Employee{
+//        String name; double salary;
+//        public  Employee(){ // constructor 1
+//            this("JOHN DOE",5000);
+//            System.out.print("Deafualt constructor called");
+//        }
+//    public  Employee(){ // constructor 2
+//        this.name=name;this.salary=salary;
+//        System.out.print("Deafualt constructor called");
+//    }
+
+    /*
+    super -> used in subclasses to refer to immediate superclass
+    class animal{
+    String color="Brown";
+    Animal(){
+    System.out.println("Animal constructor called");
+    }
+    void eat(){
+        System.out.println("Animal eats");}
+        }}
+
+        //sublclass
+        class Dog extends Animal {
+        String color="Black";
+        Dog(){
+        super();
+          System.out.println("Dog constructor called");
+        }
+        @Override
+        void eat(){
+        System.out.println("Animal eats");}
+        }
+        void color(){
+        System.out.println(this.color + super.color );}
+        }
+
+     */
+
+}
+
+}
