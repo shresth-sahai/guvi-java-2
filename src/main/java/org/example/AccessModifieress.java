807b520cf91c24d109e82047fcc95dc31264df45
shresth-sahai <shresth0407@gmail.com>
2025-02-22 10:37:58 +0530

    access Modifier

---
+++ src/main/java/org/example/AccessModifieress.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class AccessModifieress {
+    // public private protected default
+   /*
+   public -> accessible everywhere (class,method, variable)
+   in the program provided the class
+   // expose some functionality universally available
+   public class -> class is available in the package
+   public method ->is a method in public class and can be called from any other class
+   or any other class in the same package
+
+    */
+//    package org.example;
+//    public class PublicEg{
+//        public int publicVar=10;
+//        public void display(){
+//            System.out.println(publicVar);
+//        }
+//    }
+//    package com.other;
+    // import com.example.PublicEg
+
+/*
+priavte -> only accessible -> the same class where it is delcared
+// encapsulation
+
+package com.eg
+public class PrivateEg{
+//        public int priavteVar=10;
+//        public void display(){
+//            System.out.println(privateVar);
+//        }
+//    }
+
+cannot access outside the class
+
+Q How do u access private variable outside it;s class
+-> public method in the same class
+
+
+ */
+}
