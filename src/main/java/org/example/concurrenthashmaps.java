ba583601e48db86d9d81ee67bd310606e2c0f1ea
shresth-sahai <shresth0407@gmail.com>
2025-03-16 12:12:53 +0530

    wait and notify

---
+++ src/main/java/org/example/concurrenthashmaps.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class concurrenthashmaps {
+
+//    ConcurrentMap<String,Integer> hmap=new ConcurrentHashMap<>() ;
+//    hmap.putIfAbsent("A",1);hmap.putIfAbsent("B",2);
+//    hmap.replace("A",1,10);
+//
+//    ConcurrentMap<String,Integer> map=new ConcurrentHashMap<>();
+//    map.put("A",1);
+//    map.put("B",2);
+//
+//    Runnable t1=()->{
+//        map.put("A",10);
+//        System.out.print("Thread1  updated"+map);
+//    };
+//    Runnable t2=()->{
+//        map.put("B",20);
+//        System.out.print("Thread2  updated"+map);
+//    };
+
+
+
+}
