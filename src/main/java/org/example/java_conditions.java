37c909802e831facbc3cfdac5d4912436a21af42
shresth-sahai <shresth0407@gmail.com>
2025-03-29 10:48:14 +0530

    added HTML CODE

---
+++ src/main/java/org/example/java_conditions.java
@@ -0,0 +1,134 @@
+
+// condiitons -> if else 
+
+// if(condition){
+
+// }
+// else if {
+
+// }else if {
+
+// }else if {
+
+// }
+// else{
+
+// }
+int num=10;
+// if(num%2==0){
+//     System.out.print("Even no !");
+// }
+// else{
+//     System.out.print("Odd no !");
+// }
+
+// if else ladder 
+
+// int marks=10;
+
+// if(marks>=10) {
+//     System.out.print("Gretaer than 10  !");
+// }
+// else if (marks >50){
+//     System.out.print("Gretaer than 50  !");
+// }
+// else if (marks >80){
+//     System.out.print("Gretaer than 80  !");
+// }
+// else{
+//     System.out.print("!");
+// } 
+
+
+// int flag=1;
+
+// if(flag==0){
+    
+// }
+// if(flag==1){
+
+// }
+// if(flag!=1){
+
+// }
+
+// switch 
+// switch(expression){
+//     case 1:
+//     // 
+//     break; 
+
+//     case 2:
+//     // 
+//     break;
+// }
+// char operator='+';
+// int a =1; int b=4;
+// switch(operator){
+//     case '+':
+//     System.out.print(a+b);
+//     break;
+//     case '-':
+//     System.out.print(a-b);
+//     break;
+//     case '*':
+//     System.out.print(a*b);
+//     break;
+// }
+
+// loops
+// while & for -> entry controlled 
+
+for(int i=1;i<=5;i++){
+    System.out.print(i); // 1 2 3 4 5 
+}
+int sum=0; int j=1;
+
+while(sum<=10){  // while(condiiton )
+    sum+=i;
+    i++;
+}
+// do while -> exit controlled loop 
+
+// do{
+//     // code 
+// }
+// while(condition); 
+// at least execute once even befoire checking condition 
+// int input;
+// do{
+//     System.out.print("Enter a + number");
+//     input = scanner.nextInt();
+// }
+// while(input<=0);
+
+// for each loop
+// int numbers={1,2,3,4};
+// for(int n:numbers){
+//     System.out.print(n);
+// }
+
+// Sum of Even Numbers -> hardcode code -> 
+
+// int n -> check positive or negetive 
+
+
+// int sum=0;
+// for(int i=0;i<=10;i++){
+//     if(i%2==0){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+
+// int n0=-1;
+
+// if(n>0){
+//     System.out.print("posiitve no"); 
+// }
+// else{
+//     System.out.print("negetive  no"); 
+// }
+// else{
+//     System.out.print("zero"); 
+// }
