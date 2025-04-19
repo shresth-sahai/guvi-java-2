8cfc5b0c703a00b4cda94f92240401d7f92ac02f
shresth-sahai <shresth0407@gmail.com>
2025-04-19 12:45:36 +0530

    added callabcks

--- UI/intro.js
+++ UI/intro.js
@@ -285,3 +285,229 @@ now js how to run it in local -> NODE JS -> env to run js outside web browser
 
 
 
+// function a (v){
+// }
+// const a =(v) =>{
+// }
+// functions are first class citizens 
+// everything is an object 
+//hosiitng is applicable to function, classes , var
+// const func1 = () => console.log(1)
+// func1() //1 
+// func2() //2 
+// function func2() {
+//   console.log(2)
+// }
+// func3() // error 
+// // function exp 
+// var func3 = function func4() {
+//   console.log(3) 
+// }
+// IIFE -> defined and invoked immediately after creation
+
+// (function(a,b){
+//     console.log("Hi Im IIFE");
+// })();
+
+// var a = 1
+// function a() {
+// }
+
+// console.log(typeof a)
+
+// var b
+// function b() {
+// }
+// b = 1
+
+// console.log(typeof b)
+
+// function c() {
+// }
+// var c = 1;
+
+// console.log(typeof c)
+
+// var d = 1;
+
+// (function(){
+//   d = '2'
+//   console.log(typeof d)
+//   function d() {
+//   }
+// })()
+
+// console.log(typeof d)
+
+// var e = 1
+// const f = function e() {}
+
+// console.log(typeof e)
+
+// var foo = 1;
+// (function () {
+//   console.log(foo);
+//   foo = 2;
+//   console.log(window.foo);
+//   console.log(foo);
+//   var foo = 3;
+//   console.log(foo);
+//   console.log(window.foo)
+// })()
+
+
+// window is global object containing everythins
+
+// this-> execution context 
+// in browser ->
+// console.log(this) // window obj 
+// function show(){
+//     console.log(this)
+// }
+// show()
+// const p={
+//     name:'ALICE',
+//     greet: function(){
+//          console.log(this.name);
+//     }
+// }
+// p.greet();
+// arrow is not lexical scope 
+// const p={
+//     name:'ALICE',
+//     greet: () => {
+//          console.log(this.name);
+//     }
+// }
+// p.greet();
+
+
+// var foo = 1;
+// (function () {
+//   console.log(foo);
+//   foo = 2;
+//   console.log(window.foo);
+//   console.log(foo);
+//   var foo = 3;
+//   console.log(foo);
+//   console.log(window.foo)
+// })()
+
+
+
+// var bar = 1;
+// function foo() {
+//   return this.bar++
+// }
+// const a = {
+//   bar: 10,
+//   foo1: foo,
+//   foo2: function() {
+//     return foo()
+//   },
+// } 
+// console.log(a.foo1.call()) // point to window obj // 1
+// console.log(a.foo1()) //10
+// console.log(a.foo2.call())
+// console.log(a.foo2())
+
+// const obj = {
+//   a: 1,
+//   b: this.a + 1,
+//   c: () => this.a + 1,
+//   d() {
+//     return this.a + 1
+//   },
+//   e() {
+//     return (() => this.a + 1)()
+//   }
+// }
+// console.log(obj.b) // window(undefined+1)=> NAN
+// console.log(obj.c())// (undefined+1)=> NAN
+// console.log(obj.d()) // 2
+// console.log(obj.e()) //2 
+
+// var a = 1
+// function func() {
+//   a = 2
+//   console.log(a)
+//   var a
+// }
+// func() // 2 
+// console.log(a) // 1 
+// if (!('b' in window)) {
+//   var b = 1
+// }
+// console.log(b) // undefined 
+
+(() => {
+    if (!fn) {
+      function fn() {
+        console.log('2')
+      }
+    }
+    fn()
+  })()
+  function fn() {
+    console.log('1')
+  }
+  
+  function fn1() {
+    console.log('3')
+  }
+  (() => {
+    if (!fn1) {
+      function fn1() {
+        console.log('4')
+      }
+    }
+    fn1()
+  })()
+  
+  
+  // another one !
+  (() => {
+    if (false) {
+      function fn3() {
+        console.log('5')
+      }
+    }
+    fn3()
+  })()
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  // callbacks -> async 
+// func passed as an arg to another executed when 
+// function completes execution 
+// a callback is executed after task is completed
+// async function 
+// function fetchData(callback){
+//     setTimeout(()=>{
+//         const data={name:'A',age:12};
+//         callback(data);
+//     },15000);
+// }
+// function handleData(data){
+//     console.log(data);
+// }
+// fetchData(handleData);
+
+// function a() {
+//   console.log(1) //1 2 1 
+//   return {
+//     a: function() {
+//       console.log(2)
+//       return a()
+//     }
+//   }
+// }
+// a().a()
+
