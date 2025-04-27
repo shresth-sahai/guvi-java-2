4712ed1cd526dfc332fa36465b8f5af35bebda71
shresth-sahai <shresth0407@gmail.com>
2025-04-27 10:59:32 +0530

    code added

--- UI/problems.js
+++ UI/problems.js
@@ -32,4 +32,18 @@
 // [4, 3, 1, 2]
 // [4, 3, 2, 1]
 // your shuffle() should transform the array in one of the above array, at the same 1/24 probability.
+const arr = [1, 2, 3, 4];  // walmart question
+function shuffle(){
 
+    for(let i=arr.length-1;i>0;i--){
+        const randIdx=Math.floor(Math.random()*(i+1));
+        const storedItem=arr[i];
+        arr[i]=arr[randIdx];
+        arr[randIdx]=storedItem;
+    }
+return arr;
+}
+
+shuffle()// -> [1,3,4,2]
+shuffle()// -> [1,3,2,4]
+shuffle()// -> [3,1,2,4]
