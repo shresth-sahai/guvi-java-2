cd0f6b5ffff3460420898269d23b3dea30254552
shresth-sahai <shresth0407@gmail.com>
2025-04-27 11:29:45 +0530

    fetch code added

---
+++ UI/fetcsh.js
@@ -0,0 +1,23 @@
+// https://icanhazdadjoke.com
+
+// fetch 
+// axios 
+
+// fetching data from third party -> display it into our page 
+
+
+// You are building a simple weather forecast application that fetches the current weather data based on the user's location input.
+
+// The app should:
+
+// Allow users to enter a location (city name).
+
+// Display the current weather for that city, including:
+
+// City Name
+
+// Temperature
+
+// Weather Description (e.g., sunny, cloudy, etc.)
+
+// Wind Speed
